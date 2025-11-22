072f3e2fc8f04051926c120a02029e2aa4405344
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-11-22 02:00:17 +0100

    refactor: remove hard-coded project filepath in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROJECT_PATH="/Users/vartiukhov/dev/projects/samples/ij-demo"
+PROJECT_PATH=""
 echo "Running headless mode with '$PROJECT_PATH'"
 
 ./gradlew headless -Proot="$PROJECT_PATH"
