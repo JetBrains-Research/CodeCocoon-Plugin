2caaec68878c7008476fdf53265594cd2bee1a1c
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-11-25 17:39:42 +0100

    feat: implement `AddCommentTransformation` to serve as example

---
+++ src/main/kotlin/com/github/pderakhshanfar/codecocoonplugin/components/transformation/AddCommentTransformation.kt
@@ -0,0 +1,78 @@
+package com.github.pderakhshanfar.codecocoonplugin.components.transformation
+
+import com.github.pderakhshanfar.codecocoonplugin.common.Language
+import com.github.pderakhshanfar.codecocoonplugin.executor.TransformationResult
+import com.github.pderakhshanfar.codecocoonplugin.intellij.psi.document
+import com.github.pderakhshanfar.codecocoonplugin.transformation.TextBasedTransformation
+import com.intellij.openapi.application.ReadAndWriteScope
+import com.intellij.openapi.application.ReadResult
+import com.intellij.openapi.application.writeAction
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFile
+
+/**
+ * Adds a comment at the beginning of a file.
+ * Works with Java and Kotlin.
+ *
+ * **NOTE**: _serves as a mere example of how to implement a custom transformation.
+ * Note intended to be used in production_.
+ */
+class AddCommentTransformation : TextBasedTransformation(), IntelliJAwareTransformation {
+    override val name: String = "add-comment-transformation"
+    override val description: String = "Adds a comment at the beginning of the file"
+
+    override val supportedLanguages: Set<Language> = setOf(
+        Language.JAVA,
+        Language.KOTLIN,
+    )
+
+    private val message = "Hello world from $name!"
+
+    override fun apply(
+        psiFile: PsiFile,
+        virtualFile: VirtualFile,
+    ): TransformationResult {
+        val result = try {
+            val comment = createComment(psiFile.name, message)
+            val document = psiFile.document()
+
+            val value = if (document != null) {
+                // Add comment at the beginning
+                document.insertString(0, "$comment\n\n")
+                TransformationResult.Success(
+                    message = "Added comment to ${virtualFile.name}",
+                    filesModified = 1
+                )
+            } else {
+                TransformationResult.Failure(
+                    "Could not get document for file ${psiFile.name}")
+            }
+            value
+        } catch (e: Exception) {
+            TransformationResult.Failure("Failed to add comment for file ${psiFile.name}", e)
+        }
+
+        return result
+    }
+
+    /**
+     * @
+     * @param message The comment message **without** any language-specific comment prefix (e.g., "//" for Java)
+     */
+    private fun createComment(filename: String, message: String): String? {
+        val language = filename.let {
+            when {
+                it.endsWith(".java") -> Language.JAVA
+                it.endsWith(".kt") || it.endsWith(".kts") -> Language.KOTLIN
+                // defaults to Java-style comments
+                else -> Language.JAVA
+            }
+        }
+
+        return when (language) {
+            Language.JAVA, Language.KOTLIN -> "// $message"
+            else -> ""
+        }
+    }
+}
