dd0816ae569fc7d375bb5c6ed2eab1ae038dbeeb
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-11-25 17:37:23 +0100

    feat: create `JavaTransformation` interface in `java` submodule

---
+++ java/src/main/kotlin/com/github/pderakhshanfar/codecocoonplugin/java/JavaTransformation.kt
@@ -0,0 +1,12 @@
+package com.github.pderakhshanfar.codecocoonplugin.java
+
+import com.github.pderakhshanfar.codecocoonplugin.common.FileContext
+import com.github.pderakhshanfar.codecocoonplugin.common.Language
+import com.github.pderakhshanfar.codecocoonplugin.transformation.Transformation
+
+/**
+ * Marker interface for Java-specific transformations.
+ */
+interface JavaTransformation : Transformation {
+    override fun accepts(context: FileContext): Boolean = context.language == Language.JAVA
+}
