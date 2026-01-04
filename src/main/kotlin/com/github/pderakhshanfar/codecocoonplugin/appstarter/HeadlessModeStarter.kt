c7dd695e777a428b32c7795f98d488881f37a39f
dragoi75 <andrei.dragoi2002@gmail.com>
2026-01-04 16:21:15 +0200

    feat: Add transformation for renaming classes.

--- src/main/kotlin/com/github/pderakhshanfar/codecocoonplugin/appstarter/HeadlessModeStarter.kt
+++ src/main/kotlin/com/github/pderakhshanfar/codecocoonplugin/appstarter/HeadlessModeStarter.kt
@@ -1,6 +1,7 @@
 package com.github.pderakhshanfar.codecocoonplugin.appstarter
 
 import com.github.pderakhshanfar.codecocoonplugin.components.transformations.AddCommentTransformation
+import com.github.pderakhshanfar.codecocoonplugin.components.transformations.RenameClassTransformation
 import com.github.pderakhshanfar.codecocoonplugin.components.transformations.RenameMethodTransformation
 import com.github.pderakhshanfar.codecocoonplugin.components.transformations.TransformationRegistry
 import com.github.pderakhshanfar.codecocoonplugin.config.CodeCocoonConfig
@@ -136,6 +137,7 @@ class HeadlessModeStarter : ApplicationStarter {
     private fun registerBuiltInTransformations() {
         TransformationRegistry.register(AddCommentTransformation.ID) { config -> AddCommentTransformation(config) }
         TransformationRegistry.register(RenameMethodTransformation.ID) { config -> RenameMethodTransformation(config) }
+        TransformationRegistry.register(RenameClassTransformation.ID) { config -> RenameClassTransformation(config) }
     }
 
     /**
