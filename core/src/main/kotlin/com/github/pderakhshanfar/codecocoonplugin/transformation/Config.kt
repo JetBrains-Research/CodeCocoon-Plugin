25fa444aee0bd5eae0f8a81cc1ac2de494e474df
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2026-02-02 17:57:26 +0100

    feat: create `requireOrDefault` function

--- core/src/main/kotlin/com/github/pderakhshanfar/codecocoonplugin/transformation/Config.kt
+++ core/src/main/kotlin/com/github/pderakhshanfar/codecocoonplugin/transformation/Config.kt
@@ -23,3 +23,17 @@ inline fun <reified T> Map<String, Any>.require(key: String): T {
     }
     return config[key] as T
 }
+
+/**
+ * Same as [require] but returns a default value if the key is not found in the map.
+ */
+inline fun <reified T> Map<String, Any>.requireOrDefault(key: String, defaultValue: T): T {
+    val config = this
+    if (key !in config || config[key] == null) {
+        return defaultValue
+    } else if (config[key] !is T) {
+        throw IllegalArgumentException(
+            "Expected config parameter '$key' to be of type ${T::class.simpleName}")
+    }
+    return config[key] as T
+}
