bdd0bdb599492684ecbbece107d0d926ad1446f6
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2026-02-02 18:00:33 +0100

    feat: create dest directory when missing if requested in config

--- src/main/kotlin/com/github/pderakhshanfar/codecocoonplugin/components/transformations/MoveFileToNewDirectoryTransformation.kt
+++ src/main/kotlin/com/github/pderakhshanfar/codecocoonplugin/components/transformations/MoveFileToNewDirectoryTransformation.kt
@@ -6,7 +6,10 @@ import com.github.pderakhshanfar.codecocoonplugin.executor.TransformationResult
 import com.github.pderakhshanfar.codecocoonplugin.intellij.logging.withStdout
 import com.github.pderakhshanfar.codecocoonplugin.intellij.vfs.refreshAndFindVirtualFile
 import com.github.pderakhshanfar.codecocoonplugin.transformation.require
+import com.github.pderakhshanfar.codecocoonplugin.transformation.requireOrDefault
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
 
@@ -16,7 +19,8 @@ import com.intellij.psi.PsiFile
  * Expected config schema:
  * ```yaml
  * config:
- *   destination: string # a new directory where the file should be moved to
+ *   destination: string # required, a new directory where the file should be moved to
+ *   createWhenMissing: boolean (default: true) # optional, whether to create the destination directory if it doesn't exist
  * ```
  */
 class MoveFileToNewDirectoryTransformation(
@@ -27,7 +31,9 @@ class MoveFileToNewDirectoryTransformation(
 
     private val logger = thisLogger().withStdout()
 
-    private val destination: String = config.require<String>("destination")
+    // params
+    private val destination = config.require<String>("destination")
+    private val createWhenMissing = config.requireOrDefault<Boolean>("createWhenMissing", true)
 
     override fun accepts(context: FileContext): Boolean = context.language == Language.JAVA
 
@@ -35,10 +41,23 @@ class MoveFileToNewDirectoryTransformation(
         psiFile: PsiFile,
         virtualFile: VirtualFile
     ): TransformationResult {
+        val project = psiFile.project
         val requestor = this
-        val newParent = destination.refreshAndFindVirtualFile()
 
-        logger.info("Attempting to move ${virtualFile.canonicalPath} to ${newParent?.canonicalPath ?: destination} directory...")
+        logger.info("Attempting to move ${virtualFile.canonicalPath} to $destination directory...")
+
+        val newParent = destination.refreshAndFindVirtualFile() ?: when (createWhenMissing) {
+            true -> WriteCommandAction.runWriteCommandAction<VirtualFile>(project) {
+                // create the destination directory if it doesn't exist
+                logger.info("Destination directory doesn't exist. Creating it...")
+                VfsUtil.createDirectories(destination)
+            }
+            false -> {
+                logger.info("Destination directory doesn't exist. Creation on missing directory is disallowed " +
+                        "(see `createWhenMissing` in transformation config)")
+                null
+            }
+        }
 
         return when {
             newParent == null -> TransformationResult.Failure(
@@ -46,9 +65,10 @@ class MoveFileToNewDirectoryTransformation(
             !newParent.isDirectory -> TransformationResult.Failure(
                 "Cannot move ${virtualFile.canonicalPath}. Destination is not a directory: $destination")
             else -> {
-                // TODO: create destination if doesn't exist (newParent.exists)
-                // moving the file into a new directory
-                virtualFile.move(requestor, newParent)
+                WriteCommandAction.runWriteCommandAction(project) {
+                    // moving the file into a new directory
+                    virtualFile.move(requestor, newParent)
+                }
                 TransformationResult.Success("Moved ${virtualFile.canonicalPath} to $destination directory")
             }
         }
